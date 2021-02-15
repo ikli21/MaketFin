f34e0b049c66e44c17ba9dbf7449394e60ee30c4
student <@mail.ru>
2021-02-15 11:25:29 +0500

    Initial commit хуй

---
+++ app/src/main/java/com/example/maket/TrueScreen.java
@@ -0,0 +1,14 @@
+package com.example.maket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrueScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_true);
+    }
+}
